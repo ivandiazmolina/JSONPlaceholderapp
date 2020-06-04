b174faed3e5ef57aa0b95c30d071cfb482ecdc43
Iván Díaz Molina <ivandiazmolina92@gmail.com>
2020-06-04 11:19:43 +0200

    Project creation with Pod

---
+++ JSONPlaceholderapp/JSONPlaceholderapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONPlaceholderapp
+//
+//  Created by Iván Díaz Molina on 04/06/2020.
+//  Copyright © 2020 Iván Díaz Molina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
