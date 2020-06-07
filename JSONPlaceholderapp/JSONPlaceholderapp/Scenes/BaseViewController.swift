2d254de2f72b63a72585d56f6ef0f4027d5b765e
Iván Díaz Molina <ivandiazmolina92@gmail.com>
2020-06-07 09:17:50 +0200

    Added pod 'NVActivityIndicatorView' and edited .gitignore

--- JSONPlaceholderapp/JSONPlaceholderapp/Scenes/BaseViewController.swift
+++ JSONPlaceholderapp/JSONPlaceholderapp/Scenes/BaseViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
-class BaseViewController: UIViewController {
+class BaseViewController: UIViewController, NVActivityIndicatorViewable {
 
+    // properties of NVActivityIndicatorView
+    private let TEXT_LOADER = "Loading..."
+    private let TYPE_LOADER: NVActivityIndicatorType = .ballPulseSync
+    private let SIZE_LOADER = CGSize(width: 120, height: 120)
+    private let PADDING_LOADER: CGFloat = 20
+    private let COLOR_LOADER: UIColor = .white
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    /// show loading
+    func showLoading() {
+        ui { [weak self] in
+            self?.startAnimating(self?.SIZE_LOADER, message: self?.TEXT_LOADER, type: self?.TYPE_LOADER, color: self?.COLOR_LOADER, padding: self?.PADDING_LOADER)
+        }
+    }
+    
+    /// hide loading
+    func dismissLoading() {
+        ui { [weak self] in
+            self?.stopAnimating(.none)
+        }
+    }
 }
-
